b60ac25435f77c8e8850280759aa08246dfcd27e
Adamhunter108 <61752774+Adamhunter108@users.noreply.github.com>
2022-05-29 15:02:24 -0400

    adding middleweaer config

--- config/middlewares.js
+++ config/middlewares.js
@@ -27,6 +27,17 @@ module.exports = [
     },
   },
   'strapi::cors',
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
+      formidable: {
+        maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
